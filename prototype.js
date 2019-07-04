4bedca134dfa163cd9d691baf18721e9d977f520
wdv2015 <mohammadrezaei1374@hotmail.com>
2019-07-04 22:21:26 +0430

    add init files

---
+++ prototype.js
@@ -0,0 +1,24 @@
+const app = document.getElementById("changeColor");
+
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
+request.onload = function() {
+  var data = JSON.parse(this.response);
+  data.forEach(movie => {
+    const eachOff = document.createElement("div");
+    eachOff.setAttribute("class", "card");
+
+    const offCode = document.createElement("p");
+    offCode.setAttribute("class", "offCode");
+    offCode.textContent = movie.title;
+
+    container.appendChild(eachOff);
+    container.appendChild(offCode);
+  });
+};
+request.send();
