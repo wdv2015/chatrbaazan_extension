0f02f9d70a224384bd02aa5c0d137a8bc9cc4f64
shayan <yusefshayan@gmail.com>
2019-07-05 15:59:49 +0430

    added new icon, removed temp icons, removed prototype from background in manifest, created css file and removed style tags in html, made offcode a child of eachoff in prototype

--- prototype.js
+++ prototype.js
@@ -8,17 +8,17 @@ app.appendChild(container);
 var request = new XMLHttpRequest();
 request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
 request.onload = function() {
-  var data = JSON.parse(this.response);
-  data.forEach(movie => {
-    const eachOff = document.createElement("div");
-    eachOff.setAttribute("class", "card");
+	var data = JSON.parse(this.response);
+	data.forEach(movie => {
+		const eachOff = document.createElement("div");
+		eachOff.setAttribute("class", "card");
 
-    const offCode = document.createElement("p");
-    offCode.setAttribute("class", "offCode");
-    offCode.textContent = movie.title;
+		const offCode = document.createElement("p");
+		offCode.setAttribute("class", "offCode");
+		offCode.textContent = movie.title;
 
-    container.appendChild(eachOff);
-    container.appendChild(offCode);
-  });
+		container.appendChild(eachOff);
+		eachOff.appendChild(offCode);
+	});
 };
 request.send();
