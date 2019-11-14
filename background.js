f953ce8ad59c7e222514646342374eeb5678271b
mohammadrezaei <mohammadrezaei1374@hotmail.com>
2019-11-14 16:06:21 +0330

    fix : enabling extension icon problem move a paranthesis

--- background.js
+++ background.js
@@ -3,10 +3,10 @@
 // found in the LICENSE file.
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({ color: "#3aa757" }, function() {
-    console.log("The color is green.");
-  });
-
+    chrome.storage.sync.set({color: "#3aa757"}, function () {
+        console.log("The color is green.");
+    });
+});
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 
 URL2 = "digistyle.com";
@@ -36,4 +36,3 @@ chrome.tabs.onUpdated.addListener(function (){
    );
   });
   });
-});
