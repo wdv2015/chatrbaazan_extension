b0705cd37c032a7304ee7c8be5efe5c896a7ae80
mohammadrezaei <mohammadrezaei1374@hotmail.com>
2019-11-15 16:19:12 +0330

    1) expiration date validation added 2) offers detached from discount codes 3) add new element for showing full description of offers

--- prototype.js
+++ prototype.js
@@ -34,60 +34,98 @@ function toCreateTest() {
 
 
             myObj.product_company.forEach(element => {
+                if (getCuurentDate() <= element.expiration_date || element.expiration_date == null) {
                 const eachOff = document.createElement("div");
                 eachOff.setAttribute("class", "card");
                 eachOff.setAttribute("dir", "rtl");
+                //alert(element.discount_code);
+                if(element.discount_code != null) {
 
-                const desc = document.createElement("h5");
-                desc.setAttribute("class", "desc");
-                desc.textContent = element.name;
+                    const desc = document.createElement("h5");
+                    desc.setAttribute("class", "desc");
+                    desc.textContent = element.name;
 
-                // const offCode = document.createElement("p");
-                // offCode.setAttribute("class", "offCode");
-                // offCode.textContent = element.discount_code;
+                    // const offCode = document.createElement("p");
+                    // offCode.setAttribute("class", "offCode");
+                    // offCode.textContent = element.discount_code;
 
-                const offerOff = document.createElement("span");
-                offerOff.setAttribute("class", "offer_off");
-                offerOff.textContent = element.chatrbazi;
+                    const offerOff = document.createElement("span");
+                    offerOff.setAttribute("class", "offer_off");
+                    offerOff.textContent = element.chatrbazi;
 
-                const copyDiv = document.createElement("div");
-                copyDiv.setAttribute("class", "copy_div");
+                    const copyDiv = document.createElement("div");
+                    copyDiv.setAttribute("class", "copy_div");
 
-                const copyInput = document.createElement("input");
-                copyInput.setAttribute("class", "coupon_code");
-                copyInput.setAttribute("id", "inputid" + counter);
-                copyInput.setAttribute("value", element.discount_code);
-                copyInput.setAttribute("disabled", "disabled");
-                // copyInput.textContent = element.discount_code;
+                    const copyInput = document.createElement("input");
+                    copyInput.setAttribute("class", "coupon_code");
+                    copyInput.setAttribute("id", "inputid" + counter);
+                    copyInput.setAttribute("value", element.discount_code);
+                    copyInput.setAttribute("disabled", "disabled");
+                    // copyInput.textContent = element.discount_code;
 
-                const copyBtn = document.createElement("button");
-                copyBtn.setAttribute("id", "id" + counter);
-                copyBtn.setAttribute("class", "copy_button");
-                copyBtn.textContent = 'کپی';
+                    const copyBtn = document.createElement("button");
+                    copyBtn.setAttribute("id", "id" + counter);
+                    copyBtn.setAttribute("class", "copy_button");
+                    copyBtn.textContent = 'کپی';
 
-                const copyTooltip = document.createElement("span");
-                copyTooltip.setAttribute("id", "spanid" + counter);
-                copyTooltip.setAttribute("class", "copy_tooltip");
+                    const copyTooltip = document.createElement("span");
+                    copyTooltip.setAttribute("id", "spanid" + counter);
+                    copyTooltip.setAttribute("class", "copy_tooltip");
 
-                const maincont = document.createElement("div");
-                maincont.setAttribute("class", "main");
+                    const _link_ = "https://chatrbaazan.ir/offer/" + element.slug;
+
+                     const linkToMainSite = document.createElement("a");
+                    linkToMainSite.setAttribute("class" , "fullDesc");
+                    linkToMainSite.setAttribute("target" , "_blank");
+                    linkToMainSite.setAttribute("href" , _link_);
+                    linkToMainSite.textContent = "برای مشاهده ی توضیح بیشتر کلیک کنید..."
+
+                    const maincont = document.createElement("div");
+                    maincont.setAttribute("class", "main");
 
 
-                copyDiv.appendChild(copyBtn);
-                copyDiv.appendChild(copyInput);
+                    copyDiv.appendChild(copyBtn);
+                    copyDiv.appendChild(copyInput);
 
-                copyBtn.appendChild(copyTooltip);
+                    copyBtn.appendChild(copyTooltip);
 
-                eachOff.appendChild(maincont);
-                eachOff.appendChild(offerOff);
+                    eachOff.appendChild(maincont);
+                    eachOff.appendChild(offerOff);
+                    eachOff.appendChild(linkToMainSite);
 
-                maincont.appendChild(desc);
-                maincont.appendChild(copyDiv);
+                    maincont.appendChild(desc);
+                    maincont.appendChild(copyDiv);
 
-                container.appendChild(eachOff);
-                //eachOff.appendChild(offCode);
+                    container.appendChild(eachOff);
+                    //eachOff.appendChild(offCode);
 
-                counter++;
+                    counter++;
+                }else{
+                    //alert("isdfafdjsjhfjhdsjkfh");
+                    const desc = document.createElement("h5");
+                    desc.setAttribute("class", "desc");
+                    desc.textContent = element.name;
+
+                    const offerOff = document.createElement("span");
+                    offerOff.setAttribute("class", "offer_off");
+                    offerOff.textContent = element.chatrbazi;
+
+                    var _link_ = "https://chatrbaazan.ir/offer/" + element.slug;
+
+                    const linkToMainSite = document.createElement("a");
+                    linkToMainSite.setAttribute("class" , "fullDesc");
+                    linkToMainSite.setAttribute("target" , "_blank");
+                    linkToMainSite.setAttribute("href" , _link_);
+                    linkToMainSite.textContent = "برای مشاهده ی توضیح بیشتر کلیک کنید..."
+
+                    eachOff.appendChild(desc);
+                    eachOff.appendChild(offerOff);
+                    eachOff.appendChild(linkToMainSite);
+
+                    container.appendChild(eachOff);
+
+                }
+            }
 
             });
 
@@ -96,4 +134,14 @@ function toCreateTest() {
     };
     xmlhttp.open("GET", URL, true);
     xmlhttp.send();
+}
+
+function getCuurentDate(){
+    var now = new Date();
+    var dd = String(now.getDate());
+    var mm = String(now.getMonth() + 1);
+    var yyyy = String(now.getFullYear());
+
+    var today = yyyy+"-"+mm+"-"+dd+"T23:59:59+03:30";
+    return today;
 }
